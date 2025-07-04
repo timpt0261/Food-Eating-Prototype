76a319dfb8e935feb636028fcfc3ed648fd2eb1c
timpt0261 <rchavez0215@gmail.com>
2025-07-04 02:19:58 -0700

    Update HandPickUp.cs

--- Assets/Mouth Prototype/Player/Hand/HandPickUp.cs
+++ Assets/Mouth Prototype/Player/Hand/HandPickUp.cs
@@ -3,110 +3,114 @@ using UnityEngine;
 public class HandPickUp : MonoBehaviour
 {
 
-    [Header("References")]
-    private PickUpInteraction _playerPickUpInteraction;
-    private bool isPlayerHoldingObject;
+	[Header("References")]
+	private PickUpInteraction _playerPickUpInteraction;
+	private bool isPlayerHoldingObject;
 
-    [Header("Collider Handling")]
-    [SerializeField] private Transform _interactionPoint;
-    [SerializeField] private Vector3 _interactionPointSize;
-    [SerializeField] private LayerMask _interactableMask;
+	[Header("Collider Handling")]
+	[SerializeField] private Transform _interactionPoint;
+	[SerializeField] private Vector3 _interactionPointSize;
+	[SerializeField] private LayerMask _interactableMask;
 
-    private readonly Collider[] _colliders = new Collider[3];
-    private int _numfound;
+	private readonly Collider[] _colliders = new Collider[3];
+	private int _numfound;
 
-    [Header("Input Handling")]
-    [SerializeField] private bool interact;
+	[Header("Input Handling")]
+	[SerializeField] private bool interact;
 
-    private FoodObject _interactable;
-    private GameObject _interactable_Obj;
+	public bool Interact => interact;
 
+	private FoodObject _interactable;
+	private GameObject _interactable_Obj;
 
-    private void Awake()
-    {
-        _playerPickUpInteraction = GetComponent<PickUpInteraction>();
-    }
+	private Rigidbody _interactable_rb;
 
 
-    private void Update()
-    {
-        interact = Input.GetMouseButton(0);
-        _numfound = Physics.OverlapBoxNonAlloc(_interactionPoint.position, _interactionPointSize, _colliders, Quaternion.identity, _interactableMask);
-        isPlayerHoldingObject = _playerPickUpInteraction.IsHoldingObject();
-
-        Interaction();
-    }
-
-    private void Interaction()
-    {
-        if (_numfound == 0 && !isPlayerHoldingObject)
-        {
-            ClearInteract();
-            return;
-        }
-
-        if (_interactable == null && !isPlayerHoldingObject)
-        {
-            SetInteractable(_colliders[0]);
-            return;
-        }
-
-        if (_interactable != null)
-        {
-            HandleInteractable(isPlayerHoldingObject);
-            return;
-        }
-
-    }
-
-    private void ClearInteract()
-    {
-        if (_interactable != null)
-        {
-            _interactable = null;
-            _interactable_Obj = null;
-        }
-    }
-
-    private void SetInteractable(Collider collider)
-    {
-        _interactable = collider.GetComponent<FoodObject>();
-        _interactable_Obj = collider.gameObject;
-    }
+	private void Awake()
+	{
+		_playerPickUpInteraction = GetComponent<PickUpInteraction>();
+	}
 
 
-    private void HandleInteractable(bool isPlayerHoldingObject)
-    {
-        _interactable.Interact(this);
-        if (interact) { HandleInteractOn(isPlayerHoldingObject); return; }
-        HandleInteractOff(isPlayerHoldingObject);
+	private void Update()
+	{
+		interact = Input.GetMouseButton(0);
+		_numfound = Physics.OverlapBoxNonAlloc(_interactionPoint.position, _interactionPointSize, _colliders, Quaternion.identity, _interactableMask);
+		isPlayerHoldingObject = _playerPickUpInteraction.IsHoldingObject();
 
-    }
+		Interaction();
+	}
 
-    private void HandleInteractOn(bool isPlayerHoldingObject)
-    {
-        if (!isPlayerHoldingObject)
-            _playerPickUpInteraction.PickUpObject(_interactable_Obj);
-        else
-            _playerPickUpInteraction.MoveObject();
-    }
+	private void Interaction()
+	{
+		if (_numfound == 0 && !isPlayerHoldingObject)
+		{
+			ClearInteract();
+			return;
+		}
 
-    private void HandleInteractOff(bool isPlayerHoldingObject)
-    {
-        if (isPlayerHoldingObject)
-            _playerPickUpInteraction.DropObject();
-    }
+		if (_interactable == null && !isPlayerHoldingObject)
+		{
+			SetInteractable(_colliders[0]);
+			return;
+		}
 
-    private void OnDrawGizmosSelected()
-    {
-        if (_interactionPoint == null) return;
+		if (_interactable != null)
+		{
+			HandleInteractable(isPlayerHoldingObject);
+			return;
+		}
 
-        Gizmos.color = Color.yellow;
+	}
 
-        // Draw a wire cube representing the OverlapBox
-        Gizmos.matrix = Matrix4x4.TRS(_interactionPoint.position, Quaternion.identity, Vector3.one);
-        Gizmos.DrawWireCube(Vector3.zero, _interactionPointSize * 2); // Multiply by 2 because size is half-extents in OverlapBox
-    }
+	private void ClearInteract()
+	{
+		if (_interactable != null)
+		{
+			_interactable = null;
+			_interactable_Obj = null;
+		}
+	}
+
+	private void SetInteractable(Collider collider)
+	{
+		_interactable = collider.GetComponent<FoodObject>();
+		_interactable_Obj = collider.gameObject;
+	}
+
+
+	private void HandleInteractable(bool isPlayerHoldingObject)
+	{
+		_interactable.Interact(this);
+		if (interact) { HandleInteractOn(isPlayerHoldingObject); return; }
+		HandleInteractOff(isPlayerHoldingObject);
+
+	}
+
+	private void HandleInteractOn(bool isPlayerHoldingObject)
+	{
+		if (!isPlayerHoldingObject)
+			_playerPickUpInteraction.PickUpObject(_interactable_Obj);
+		else
+			_playerPickUpInteraction.MoveObject();
+	}
+
+	private void HandleInteractOff(bool isPlayerHoldingObject)
+	{
+		if (isPlayerHoldingObject)
+			_playerPickUpInteraction.DropObject();
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		if (_interactionPoint == null) return;
+
+		Gizmos.color = Color.yellow;
+
+		// Draw a wire cube representing the OverlapBox
+		Gizmos.matrix = Matrix4x4.TRS(_interactionPoint.position, Quaternion.identity, Vector3.one);
+		Gizmos.DrawWireCube(Vector3.zero, _interactionPointSize * 2); // Multiply by 2 because size is half-extents in OverlapBox
+	}
 
 
 }
