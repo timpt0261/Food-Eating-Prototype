d2ad5c8b92e42df1632f0b797657b3e417fd0eab
timpt0261 <rchavez0215@gmail.com>
2025-07-08 20:08:39 -0700

    Add TMP Pro and Reorganized Files

---
+++ Assets/Mouth Prototype/Player/Stats/CharacterStats.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterStats", menuName = "Scriptable Objects/CharacterStats")]
+public class CharacterStats : ScriptableObject
+{
+
+	public int Health;
+
+	public int Strength;
+
+	public int Speed;
+    
+}
